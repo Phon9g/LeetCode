2f57e7d67575426a3abf282f4e56c46c431c5ab4
Huynh Hoang Phong <Phon9g@gmail.com>
2025-04-07 23:46:58 +0700

    Create HappyNumber.cpp

---
+++ HappyNumber.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool isHappy(int n) {
+        unordered_set<int> seen;
+
+        while (n != 1 && seen.find(n) == seen.end()) {
+            seen.insert(n);
+            n = getNext(n);
+        }
+
+        return n == 1;
+    }
+
+private:
+    int getNext(int num) {
+        int total = 0;
+        while (num > 0) {
+            int digit = num % 10;
+            total += digit * digit;
+            num /= 10;
+        }
+        return total;
+    }
+};
