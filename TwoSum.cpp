8b4dca97e528ae1b68c03e8c1cc09db7d934a225
Huynh Hoang Phong <Phon9g@gmail.com>
2025-05-14 12:25:54 +0700

    Update TwoSum.cpp

--- TwoSum.cpp
+++ TwoSum.cpp
@@ -1,16 +1,21 @@
 class Solution {
-public:
-    vector<int> twoSum(vector<int>& nums, int target) {
-        unordered_map<int, int> hash;
-        for (int i = 0; i < nums.size(); i++) {
-            hash[nums[i]] = i;
-        }
-        for (int i = 0; i < nums.size(); i++) {
-            int complement = target - nums[i];
-            if (hash.find(complement) != hash.end() && hash[complement] != i) {
-                return {i, hash[complement]};
+    public:
+    vector<int> twoSum(vector<int> &nums, int target){
+        for (int i = 0; i < nums.size(); i ++){
+            for (int j = i + 1; j < nums.size(); j ++ ){
+                if ( nums[i] + nums[j] == target){
+                    return {i, j};
+                }
             }
         }
         return {};
     }
 };
+
+// ý tưởng 
+// Dùng 2 vòng lặp 
+// Duyệt lần lượt 2 phần tử nếu tổng 2 phần tử bằng target thì return
+/*
+input: nums [1,2,3,4] , target = 3
+output: [0,1]
+    */
